86afe0d07dc0a3ae819037fb054a1832496daa84
Petar Virijević <wiriyevich@gmail.com>
2024-12-02 16:38:18 +0100

    update usersService to use password encoder

--- src/main/java/com/wirke/jobportal/services/UsersService.java
+++ src/main/java/com/wirke/jobportal/services/UsersService.java
@@ -3,6 +3,7 @@ package com.wirke.jobportal.services;
 import java.sql.Date;
 import java.util.Optional;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.wirke.jobportal.entity.JobSeekerProfile;
@@ -18,19 +19,25 @@ public class UsersService {
     private final UsersRepository usersRepository;
     private final RecruiterProfileRepository recruiterRepository;
     private final JobSeekerProfileRepository jobSeekerRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public UsersService(UsersRepository usersRepository,
             RecruiterProfileRepository recruiterRepository,
-            JobSeekerProfileRepository jobSeekerRepository){
+            JobSeekerProfileRepository jobSeekerRepository,
+            PasswordEncoder passwordEncoder){
+
         this.usersRepository = usersRepository;
         this.jobSeekerRepository = jobSeekerRepository;
         this.recruiterRepository = recruiterRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public Users addNew(Users users){
         
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
+        users.setPassword(passwordEncoder.encode(users.getPassword()));
+
         Users savedUser = usersRepository.save(users);
 
         int userTypeId = users.getUserTypeId().getUserTypeId();
