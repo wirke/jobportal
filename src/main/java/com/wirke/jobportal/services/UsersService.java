d2e95271a1f6958f8199b60bd78ea7038f8d4441
Petar Virijević <wiriyevich@gmail.com>
2024-12-02 15:17:09 +0100

    modified to use recruiter and jobSeeker services

--- src/main/java/com/wirke/jobportal/services/UsersService.java
+++ src/main/java/com/wirke/jobportal/services/UsersService.java
@@ -5,23 +5,43 @@ import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
+import com.wirke.jobportal.entity.JobSeekerProfile;
+import com.wirke.jobportal.entity.RecruiterProfile;
 import com.wirke.jobportal.entity.Users;
+import com.wirke.jobportal.repository.JobSeekerProfileRepository;
+import com.wirke.jobportal.repository.RecruiterProfileRepository;
 import com.wirke.jobportal.repository.UsersRepository;
 
 @Service
 public class UsersService {
     
     private final UsersRepository usersRepository;
+    private final RecruiterProfileRepository recruiterRepository;
+    private final JobSeekerProfileRepository jobSeekerRepository;
 
-    public UsersService(UsersRepository usersRepository){
+    public UsersService(UsersRepository usersRepository,
+            RecruiterProfileRepository recruiterRepository,
+            JobSeekerProfileRepository jobSeekerRepository){
         this.usersRepository = usersRepository;
+        this.jobSeekerRepository = jobSeekerRepository;
+        this.recruiterRepository = recruiterRepository;
     }
 
     public Users addNew(Users users){
         
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return usersRepository.save(users);
+        Users savedUser = usersRepository.save(users);
+
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+        
+        if(userTypeId == 1){
+            recruiterRepository.save(new RecruiterProfile(savedUser));
+        } else {
+            jobSeekerRepository.save(new JobSeekerProfile(savedUser));
+        }
+
+        return savedUser;
     }
 
     public Optional<Users> getUserByEmail(String email){
