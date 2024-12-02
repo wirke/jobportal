294ef2f4ba479561b7a50c65c41cfbdd5ab727fd
Petar Virijević <wiriyevich@gmail.com>
2024-12-02 12:38:31 +0100

    added a check for user email to repository

--- src/main/java/com/wirke/jobportal/repository/UsersRepository.java
+++ src/main/java/com/wirke/jobportal/repository/UsersRepository.java
@@ -1,10 +1,12 @@
 package com.wirke.jobportal.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.wirke.jobportal.entity.Users;
 
 public interface UsersRepository extends JpaRepository<Users, Integer>{
     
-    
+    Optional<Users> findByEmail(String email);
 }
