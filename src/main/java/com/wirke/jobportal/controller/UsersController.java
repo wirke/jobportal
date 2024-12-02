49751d62bcdbfdcbda58d303984b4d94e3ae59bc
Petar Virijević <wiriyevich@gmail.com>
2024-12-02 12:19:59 +0100

    added registration controller for new users using UserService

--- src/main/java/com/wirke/jobportal/controller/UsersController.java
+++ src/main/java/com/wirke/jobportal/controller/UsersController.java
@@ -5,18 +5,24 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.wirke.jobportal.entity.Users;
 import com.wirke.jobportal.entity.UsersType;
+import com.wirke.jobportal.services.UsersService;
 import com.wirke.jobportal.services.UsersTypeService;
+
+import jakarta.validation.Valid;
 
 @Controller
 public class UsersController {
     
     private final UsersTypeService usersTypeService;
+    private final UsersService usersService;
 
-    public UsersController(UsersTypeService usersTypeService) {
+    public UsersController(UsersTypeService usersTypeService, UsersService usersService) {
         this.usersTypeService = usersTypeService;
+        this.usersService = usersService;
     }
 
     @GetMapping("/register")
@@ -25,5 +31,13 @@ public class UsersController {
         model.addAttribute("getAllTypes", usersTypes);
         model.addAttribute("user", new Users());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String userRegistration(@Valid Users users){
+
+        //System.out.println("User:: "+users);
+        usersService.addNew(users);
+        return "dashboard";
     }
 }
