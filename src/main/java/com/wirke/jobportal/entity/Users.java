7dfe2adb85dfe6d484e98cddc5f8ba9c6f2048c5
Petar Virijević <wiriyevich@gmail.com>
2024-12-02 12:19:33 +0100

    fixed generation type to Identity

--- src/main/java/com/wirke/jobportal/entity/Users.java
+++ src/main/java/com/wirke/jobportal/entity/Users.java
@@ -20,7 +20,7 @@ import jakarta.validation.constraints.NotEmpty;
 public class Users {
     
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int user_id; 
 
     @Column(unique = true)
