a422c627dc5a1816a1309f4e87f4b61e8386d34c
Petar Virijević <wiriyevich@gmail.com>
2024-12-03 16:11:11 +0100

    added addNew method for profile management

--- src/main/java/com/wirke/jobportal/services/RecruiterProfileService.java
+++ src/main/java/com/wirke/jobportal/services/RecruiterProfileService.java
@@ -20,4 +20,9 @@ public class RecruiterProfileService {
 
         return recruiterProfileRepository.findById(id);
     }
+
+    public RecruiterProfile addNew(RecruiterProfile recruiterProfile){
+
+        return recruiterProfileRepository.save(recruiterProfile);
+    }
 }
