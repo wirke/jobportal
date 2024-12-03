ca090102c1206755d3806788d14dc43ca131e1f8
Petar Virijević <wiriyevich@gmail.com>
2024-12-03 11:47:18 +0100

    added service for finding a recruiter

---
+++ src/main/java/com/wirke/jobportal/services/RecruiterProfileService.java
@@ -0,0 +1,23 @@
+package com.wirke.jobportal.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.wirke.jobportal.entity.RecruiterProfile;
+import com.wirke.jobportal.repository.RecruiterProfileRepository;
+
+@Service
+public class RecruiterProfileService {
+    
+    private final RecruiterProfileRepository recruiterProfileRepository;
+
+    public RecruiterProfileService(RecruiterProfileRepository recruiterProfileRepository) {
+        this.recruiterProfileRepository = recruiterProfileRepository;
+    }
+
+    public Optional<RecruiterProfile> getOne(Integer id){
+
+        return recruiterProfileRepository.findById(id);
+    }
+}
