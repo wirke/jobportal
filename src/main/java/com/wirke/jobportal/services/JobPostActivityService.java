8b1993a603da2402106f6cc8face30ff8533e8df
Petar Virijević <wiriyevich@gmail.com>
2024-12-04 14:43:15 +0100

    added getOne method for getting a job by id

--- src/main/java/com/wirke/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/wirke/jobportal/services/JobPostActivityService.java
@@ -39,4 +39,10 @@ public class JobPostActivityService {
         }
         return recruiterJobsDtoList;
     }
+
+    public JobPostActivity getOne(int id) {
+        
+        return jobPostActivityRepository.findById(id).orElseThrow(() ->
+            new RuntimeException("Job not found"));
+    }
 }
