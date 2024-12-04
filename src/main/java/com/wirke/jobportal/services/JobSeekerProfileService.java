f6dcb909b6f7d6a323e173565872bbb7cf7cd469
Petar Virijević <wiriyevich@gmail.com>
2024-12-04 16:24:39 +0100

    added service for candidate and  method for getting one by id

---
+++ src/main/java/com/wirke/jobportal/services/JobSeekerProfileService.java
@@ -0,0 +1,22 @@
+package com.wirke.jobportal.services;
+
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.wirke.jobportal.entity.JobSeekerProfile;
+import com.wirke.jobportal.repository.JobSeekerProfileRepository;
+
+@Service
+public class JobSeekerProfileService {
+
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
+
+    public JobSeekerProfileService(JobSeekerProfileRepository jobSeekerProfileRepository) {
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
+    }
+
+    public Optional<JobSeekerProfile> getOne(Integer id){
+        return jobSeekerProfileRepository.findById(id);
+    }
+}
