15c4d58e6491072613d2798582be679bca516437
Petar Virijević <wiriyevich@gmail.com>
2024-12-04 14:16:29 +0100

    implemented DTO for recruiter in recruiter service

--- src/main/java/com/wirke/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/wirke/jobportal/services/JobPostActivityService.java
@@ -1,8 +1,15 @@
 package com.wirke.jobportal.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.wirke.jobportal.entity.JobCompany;
+import com.wirke.jobportal.entity.JobLocation;
 import com.wirke.jobportal.entity.JobPostActivity;
+import com.wirke.jobportal.entity.DTO.RecruiterJobsDto;
+import com.wirke.jobportal.entity.Interface.IRecruiterJobs;
 import com.wirke.jobportal.repository.JobPostActivityRepository;
 
 @Service
@@ -17,5 +24,19 @@ public class JobPostActivityService {
     public JobPostActivity addNew(JobPostActivity jobPostActivity){
         
         return jobPostActivityRepository.save(jobPostActivity);
+    }
+
+    public List<RecruiterJobsDto> getRecruiterJobs(int recruiter){
+        
+        List<IRecruiterJobs> recruiterJobsDtos =  jobPostActivityRepository.getRecruiterJobs(recruiter);
+        List<RecruiterJobsDto> recruiterJobsDtoList = new ArrayList<>();
+
+        for (IRecruiterJobs rec:recruiterJobsDtos){
+            JobLocation loc = new JobLocation(rec.getLocationId(), rec.getCity(), rec.getState(), rec.getCountry());
+            JobCompany comp = new JobCompany(rec.getCompanyId(), rec.getName(), "");
+            recruiterJobsDtoList.add(new RecruiterJobsDto( rec.getTotalCandidates(), rec.getJob_post_id(), 
+                rec.getJob_title(), loc, comp));
+        }
+        return recruiterJobsDtoList;
     }
 }
