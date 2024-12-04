feb98b9d50a1d6aa72719d1567164e61bb38529f
Petar Virijević <wiriyevich@gmail.com>
2024-12-04 14:23:00 +0100

    added interface for adding job in recruiter service

---
+++ src/main/java/com/wirke/jobportal/entity/Interface/IRecruiterJobs.java
@@ -0,0 +1,22 @@
+package com.wirke.jobportal.entity.Interface;
+
+public interface IRecruiterJobs {
+
+    Long getTotalCandidates();
+
+    int getJob_post_id();
+
+    String getJob_title();
+
+    int getLocationId();
+
+    String getCity();
+
+    String getState();
+
+    String getCountry();
+
+    int getCompanyId();
+
+    String getName();
+}
