2e64d648b570314b88f12114a8efa073498e0fc2
Petar Virijević <wiriyevich@gmail.com>
2024-12-04 15:05:11 +0100

    added a route for viewing a job

---
+++ src/main/java/com/wirke/jobportal/controller/JobSeekerApplyController.java
@@ -0,0 +1,32 @@
+package com.wirke.jobportal.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.wirke.jobportal.entity.JobPostActivity;
+import com.wirke.jobportal.services.JobPostActivityService;
+import com.wirke.jobportal.services.UsersService;
+
+@Controller
+public class JobSeekerApplyController {
+    
+    private final JobPostActivityService jobPostActivityService;
+    private final UsersService usersService;
+
+    public JobSeekerApplyController(JobPostActivityService jobPostActivityService, UsersService usersService) {
+        this.jobPostActivityService = jobPostActivityService;
+        this.usersService = usersService;
+    }
+
+    @GetMapping("/job-details-apply/{id}")
+    public String display(@PathVariable("id") int id, Model model){
+
+        JobPostActivity jobDetails = jobPostActivityService.getOne(id);
+
+        model.addAttribute("jobDetails", jobDetails);
+        model.addAttribute("user", usersService.getCurrentUserProfile());
+        return "job-details";
+    }
+}
