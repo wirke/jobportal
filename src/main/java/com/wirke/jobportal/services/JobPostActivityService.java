b5c4a8b8204182399885fa16113d37d4db51fc11
Petar Virijević <wiriyevich@gmail.com>
2024-12-05 11:43:15 +0100

    added getAll method in service for jobs

--- src/main/java/com/wirke/jobportal/services/JobPostActivityService.java
+++ src/main/java/com/wirke/jobportal/services/JobPostActivityService.java
@@ -1,7 +1,9 @@
 package com.wirke.jobportal.services;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 
@@ -44,5 +46,18 @@ public class JobPostActivityService {
         
         return jobPostActivityRepository.findById(id).orElseThrow(() ->
             new RuntimeException("Job not found"));
+    }
+
+    public List<JobPostActivity> getAll() {
+
+        return jobPostActivityRepository.findAll();
+    }
+
+    public List<JobPostActivity> search(String job, String location, List<String> type, List<String> remote,
+            LocalDate searchDate) {
+        
+        return Objects.isNull(searchDate) ? 
+            jobPostActivityRepository.searchWithoutDate(job, location, remote, type) :
+            jobPostActivityRepository.search(job, location, remote,  type, searchDate);
     }
 }
