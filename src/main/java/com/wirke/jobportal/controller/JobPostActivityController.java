58b7d6f3560c69126120e0dfb87dccde81cc04c8
Petar Virijević <wiriyevich@gmail.com>
2024-12-05 11:30:00 +0100

    updated route / to have input params

--- src/main/java/com/wirke/jobportal/controller/JobPostActivityController.java
+++ src/main/java/com/wirke/jobportal/controller/JobPostActivityController.java
@@ -1,7 +1,10 @@
 package com.wirke.jobportal.controller;
 
 import java.sql.Date;
+import java.time.LocalDate;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -12,29 +15,99 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.wirke.jobportal.entity.JobPostActivity;
+import com.wirke.jobportal.entity.JobSeekerApply;
+import com.wirke.jobportal.entity.JobSeekerProfile;
 import com.wirke.jobportal.entity.RecruiterProfile;
 import com.wirke.jobportal.entity.Users;
 import com.wirke.jobportal.entity.DTO.RecruiterJobsDto;
 import com.wirke.jobportal.services.JobPostActivityService;
+import com.wirke.jobportal.services.JobSeekerApplyService;
 import com.wirke.jobportal.services.UsersService;
+
+import org.springframework.util.StringUtils;
 
 @Controller
 public class JobPostActivityController {
     
     private final UsersService usersService;
     private final JobPostActivityService jobPostActivityService;
+    private final JobSeekerApplyService jobSeekerApplyService;
 
-
-
-    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService) {
+    public JobPostActivityController(UsersService usersService, JobPostActivityService jobPostActivityService,
+            JobSeekerApplyService jobSeekerApplyService) {
         this.usersService = usersService;
         this.jobPostActivityService = jobPostActivityService;
+        this.jobSeekerApplyService = jobSeekerApplyService;
     }
 
     @GetMapping("/dashboard/")
-    public String searchJobs(Model model){
+    public String searchJobs(Model model, 
+                            @RequestParam(value = "job", required = false) String job,
+                            @RequestParam(value = "location", required = false) String location,
+                            @RequestParam(value = "partTime", required = false) String partTime,
+                            @RequestParam(value = "fullTime", required = false) String fullTime,
+                            @RequestParam(value = "freelance", required = false) String freelance,
+                            @RequestParam(value = "remoteOnly", required = false) String remoteOnly,
+                            @RequestParam(value = "officeOnly", required = false) String officeOnly,
+                            @RequestParam(value = "partialRemote", required = false) String partialRemote,
+                            @RequestParam(value = "today", required = false) boolean today,
+                            @RequestParam(value = "days7", required = false) boolean days7,
+                            @RequestParam(value = "days30", required = false) boolean days30){
+
+        model.addAttribute("partTime", Objects.equals(partTime, "Part-Time"));
+        model.addAttribute("fullTime", Objects.equals(fullTime, "Full-Time"));
+        model.addAttribute("freelance", Objects.equals(freelance, "Freelance"));
+
+        model.addAttribute("remoteOnly", Objects.equals(remoteOnly, "Remote-Only"));
+        model.addAttribute("officeOnly", Objects.equals(officeOnly, "Office-Only"));
+        model.addAttribute("partialRemote", Objects.equals(partialRemote, "Partial-Remote"));
+
+        model.addAttribute("today", today);
+        model.addAttribute("days7", days7);
+        model.addAttribute("days30", days30);
+        
+        model.addAttribute("job", job);
+        model.addAttribute("location", location);
+
+        LocalDate searchDate = null;
+        List<JobPostActivity> jobPost = null;
+        boolean dateSearchFlag = true;
+        boolean remote = true;
+        boolean type = true;
+
+        if(days30){
+            searchDate = LocalDate.now().minusDays(30);
+        } else if (days7) {
+            searchDate = LocalDate.now().minusDays(7);
+        } else if (today) {
+            searchDate = LocalDate.now();
+        } else {
+            dateSearchFlag = false;
+        }
+
+        if(partTime == null && fullTime == null && freelance == null){
+            partTime = "Part-Time";
+            fullTime = "Full-Time";
+            freelance = "Freelance";
+        }
+
+        if(officeOnly == null && remoteOnly == null && partialRemote == null){
+            officeOnly = "Office-Only";
+            remoteOnly = "Remote-Only";
+            partialRemote = "Partial-Remote";
+            type = false;
+        }
+
+        if (!dateSearchFlag && !remote && !type && !StringUtils.hasText(job) && !StringUtils.hasText(location)){
+
+            jobPost = jobPostActivityService.getAll();
+        } else {
+            jobPost = jobPostActivityService.search(job, location, Arrays.asList(partTime, fullTime, freelance),
+                Arrays.asList(remoteOnly, officeOnly, partialRemote), searchDate);
+        }
 
         Object currentUserProfile = usersService.getCurrentUserProfile();
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
