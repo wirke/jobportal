87cb431feea9333bbaab8b845bd7e32d9d929358
Petar Virijević <wiriyevich@gmail.com>
2024-12-05 11:49:57 +0100

    created service for applying to job

---
+++ src/main/java/com/wirke/jobportal/services/JobSeekerApplyService.java
@@ -0,0 +1,28 @@
+package com.wirke.jobportal.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.wirke.jobportal.entity.JobPostActivity;
+import com.wirke.jobportal.entity.JobSeekerApply;
+import com.wirke.jobportal.entity.JobSeekerProfile;
+import com.wirke.jobportal.repository.JobSeekerApplyRepository;
+
+@Service
+public class JobSeekerApplyService {
+    
+    private final JobSeekerApplyRepository jobSeekerApplyRepository;
+
+    public JobSeekerApplyService(JobSeekerApplyRepository jobSeekerApplyRepository) {
+        this.jobSeekerApplyRepository = jobSeekerApplyRepository;
+    }
+
+    public List<JobSeekerApply> getCandidatesJobs(JobSeekerProfile userAccoundId){
+        return jobSeekerApplyRepository.findByUserId(userAccoundId);
+    }
+    
+    public List<JobSeekerApply> getJobCandidates(JobPostActivity job){
+        return jobSeekerApplyRepository.findByJob(job);
+    }
+}
