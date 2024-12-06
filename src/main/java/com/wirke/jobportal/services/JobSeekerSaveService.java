3d4c08a5f7b12e8aab39b584dec2c84e5abd3c7f
Petar Virijević <wiriyevich@gmail.com>
2024-12-06 11:30:09 +0100

    Update JobSeekerSaveService.java

--- src/main/java/com/wirke/jobportal/services/JobSeekerSaveService.java
+++ src/main/java/com/wirke/jobportal/services/JobSeekerSaveService.java
@@ -1,13 +1,17 @@
 package com.wirke.jobportal.services;
 
+import java.util.ConcurrentModificationException;
 import java.util.List;
 
+import org.springframework.orm.ObjectOptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
 
 import com.wirke.jobportal.entity.JobPostActivity;
 import com.wirke.jobportal.entity.JobSeekerProfile;
 import com.wirke.jobportal.entity.JobSeekerSave;
 import com.wirke.jobportal.repository.JobSeekerSaveRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class JobSeekerSaveService {
@@ -24,4 +28,13 @@ public class JobSeekerSaveService {
     public List<JobSeekerSave> getJobCandidates(JobPostActivity job){
         return jobSeekerSaveRepository.findByJob(job);
     }
+
+    @Transactional
+    public void addNew(JobSeekerSave jobSeekerSave) {
+        try {
+            jobSeekerSaveRepository.save(jobSeekerSave);
+        } catch (ObjectOptimisticLockingFailureException ex) {
+            throw new ConcurrentModificationException("This record was modified by someone else.");
+    }
+}
 }
